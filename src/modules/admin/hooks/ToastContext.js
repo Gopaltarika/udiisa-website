22a3c18562243b52b01ff98e92956af898bc534f
gopal <punjabijanu586@gmail.com>
2026-03-04 10:04:01 +0530

    update all code

--- src/modules/admin/hooks/ToastContext.js
+++
@@ -1,4 +0,0 @@
-// admin/hooks/ToastContext.js
-import { createContext, useContext } from 'react'
-export const ToastContext = createContext(null)
-export const useAdminToast = () => useContext(ToastContext)
